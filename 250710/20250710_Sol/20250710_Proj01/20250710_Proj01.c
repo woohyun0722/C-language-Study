3347a1a6ec86a3d9d0facc7d97fd89e51f4935a2
woohyun0722 <woohyun060722@gmail.com>
2025-07-15 21:50:49 +0900

    250715

---
+++ 250710/20250710_Sol/20250710_Proj01/20250710_Proj01.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+void say_hello() {
+	int x = 1;
+	x = 10;
+	printf("I Love C");
+	return;
+}
+
+int main() {
+	/*say_hello();*/
+
+	int x, y, z;
+	x = 1;
+	y = 20;
+	z = 3;
+
+	z = x + y;
+
+	say_hello();
+
+	return 0;
+}		
---
+++ 250710/20250710_Sol/20250710_Proj02/20250710_Proj02.c
@@ -0,0 +1,18 @@
+﻿//int main() {
+//	int n1, n2, n3, n4;
+//
+//	n1 = 1;
+//	n2 = n1 * n1;
+//	n3 = n2 * n1;
+//	n4 = n3 * n1;
+//
+//
+//	return 0;
+//}
+
+int main() {
+	int x = 0, y = 1, z = 0;
+
+	z = x + y;
+	return 0;
+}
